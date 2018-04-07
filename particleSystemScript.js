a90e33ab5b07b5f3b5fd09de55566f7a8f4f42ef
AdityaAgg <agga140@usc.edu>
2018-04-07 03:32:49 -0700

    sprite rendering and color

--- particleSystemScript.js
+++ particleSystemScript.js
@@ -3,8 +3,10 @@ let f01DragCoef = 0.1;
 let attractionPts = [];
 //data
 let position = [];
+let positionsData = [];
 let delta = [];
 let color = [];
+let textureCoords = [];
 
 
 let slowHue = 0.0;
@@ -15,6 +17,7 @@ let fastSaturation = 1.0;
 let fastValue = 1.0;
 let hueDirection = 1; //0 for clockwise and 1 for counterclockwise
 
+let spriteDim = 6;
 
 
 //constants
@@ -54,13 +57,17 @@ function main() {
   canvas.style.width =  w;
 
   // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+  const gl = canvas.getContext("webgl",  {
+    premultipliedAlpha: false  // Ask for non-premultiplied alpha
+  });
 
   // Only continue if WebGL is available and working
   if (!gl) {
     alert("Unable to initialize WebGL. Your browser or machine may not support it.");
     return;
   }
+
+
 
   var handleStart = function(evt) {
     evt.preventDefault();
@@ -170,16 +177,19 @@ function main() {
     //varying lowp vec4 vColor;
   const vsSource = `
     attribute vec4 aVertexPosition;
+    attribute vec2 aTextureCoord;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
     attribute vec4 aVertexColor;
     varying lowp vec4 vColor;
+    varying lowp vec2 vTextureCoord;
 
 
 
     void main() {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
       vColor = aVertexColor;
+      vTextureCoord = aTextureCoord;
       gl_PointSize = 1.0;
     }
   `;
@@ -187,10 +197,12 @@ function main() {
   //varying lowp vec4 vColor;
   const fsSource = `
       varying lowp vec4 vColor;
-
+      varying lowp vec2 vTextureCoord;
+      uniform sampler2D uSampler;
       void main() {
 
-        gl_FragColor = vColor;
+        gl_FragColor =  vec4(texture2D(uSampler, vTextureCoord).r * 0.4 + vColor.r * 0.6, texture2D(uSampler, vTextureCoord).g * 0.4 + vColor.g * 0.6, texture2D(uSampler, vTextureCoord).b * 0.4 + vColor.b * 0.6, texture2D(uSampler, vTextureCoord).a);
+
       }
     `;
 
@@ -202,10 +214,12 @@ function main() {
     attribLocations: {
       vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
       vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
     },
     uniformLocations: {
       projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
       modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
     },
   };
 
@@ -216,20 +230,23 @@ function main() {
    initParticles(400, 400);
    //updateParticle(0);
    //Draw the scene repeatedly
+
+   const texture = loadTexture(gl, 'img/sprite2.png');
    function render(now) {
      for(let i = 0; i<delta.length; ++i) {
        updateParticle(i);
 
 
      }
-
      const buffers = initBuffers(gl);
+
+
      //console.log(color);
      now *= 0.001;  // convert to seconds
      const deltaTime = now - then;
      then = now;
 
-     drawScene(gl, programInfo, buffers, deltaTime);
+     drawScene(gl, programInfo, buffers, deltaTime, texture);
 
      requestAnimationFrame(render);
    }
@@ -241,8 +258,60 @@ function main() {
 }
 
 
+//Loading Texture function
+//Found from: https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Using_textures_in_WebGL
+//for loading texture
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
 
+  // Because images have to be download over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
 
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn of mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.crossOrigin = "Anonymous";
+  image.src = url;
+
+  //image.load();
+  return texture;
+}
+
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
+}
 
 
 
@@ -411,18 +480,48 @@ function initParticles(width, height) {
     r = radius * Math.sqrt(Math.random())
 
     theta = Math.random() * Math.PI * 2;
+    let x = (r * Math.cos(theta));
+    let y = r * Math.sin(theta) + 1;
+    position.push(x);
+    position.push(y);
 
-    position.push((r * Math.cos(theta)));
-    position.push((r * Math.sin(theta) + 1));
+    positionsData.push(x - spriteDim);
+    positionsData.push(y - spriteDim);
+
+    positionsData.push(x - spriteDim);
+    positionsData.push(y + spriteDim);
+
+    positionsData.push(x + spriteDim);
+    positionsData.push(y + spriteDim);
+
+    positionsData.push(x + spriteDim);
+    positionsData.push(y + spriteDim);
+
+    positionsData.push(x + spriteDim);
+    positionsData.push(y - spriteDim);
+
+    positionsData.push(x - spriteDim);
+    positionsData.push(y - spriteDim);
+
+    Array.prototype.push.apply(textureCoords, [0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0]);
+
+
+
+
 
     delta.push(vec2.fromValues(0.0, 0.0));
     speedCoef = getSpeedCoef(delta[i]);
 
     let c = hsv2rgba(getHue(speedCoef), getSaturation(speedCoef), getValue(speedCoef));
-    color.push(c[0]);
-    color.push(c[1]);
-    color.push(c[2]);
-    color.push(c[3]);
+
+    for (let num = 0; num<6; ++num)
+    {
+      color.push(c[0]);
+      color.push(c[1]);
+      color.push(c[2]);
+      color.push(c[3]);
+    }
+
   }
 }
 
@@ -466,16 +565,24 @@ function updateParticle(index) {
 
   position[index * offsetFactor] += delta[index][0];
   position[index * offsetFactor + 1] += delta[index][1];
+
+  //update positions data
+  for (let i = 0; i<6; ++i) {
+
+    positionsData[index * 12 + i * offsetFactor] += delta[index][0];
+    positionsData[index * 12 + i * offsetFactor + 1] += delta[index][1];
+  }
+
   speedCoef = getSpeedCoef(delta[index]);
   let c = hsv2rgba(getHue(speedCoef), getSaturation(speedCoef), getValue(speedCoef));
   //color[index] = c;
 
 
 
-  color[index * 4] = c[0];
-  color[index * 4  + 1] = c[1];
-  color[index * 4 + 2] = c[2];
-  color[index * 4 + 3] =  c[3];
+  color[index * 4 * 6] = c[0];
+  color[index * 4 * 6  + 1] = c[1];
+  color[index * 4 * 6 + 2] = c[2];
+  color[index * 4 * 6 + 3] =  c[3];
 
 
   vec2.scale(delta[index],  delta[index], f01DragCoef);
@@ -487,16 +594,17 @@ function updateParticle(index) {
 
 
 
-function drawScene(gl, programInfo, buffers, deltaTime) {
+function drawScene(gl, programInfo, buffers, deltaTime, texture) {
 
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
   gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
   // Clear the canvas before we start drawing on it.
-
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   // Create a perspective matrix, a special matrix that is
   // used to simulate the distortion of perspective in a camera.
@@ -551,7 +659,33 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
         offset);
     gl.enableVertexAttribArray(
         programInfo.attribLocations.vertexPosition);
+
   }
+
+
+  //texture coords
+  {
+    const numComponents = 2;  // pull out 2 values per iteration
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to  from one set of values to the next
+                              // 0 = use type and numComponents above
+    const offset = 0;         // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.textureCoord,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.textureCoord);
+
+  }
+
+
+
 
 
   //Tell WebGL how to pull out colors from color
@@ -593,11 +727,19 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
       modelViewMatrix);
 
 
+  //set texture
+  // Tell WebGL we want to affect texture unit 0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture to texture unit 0
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // Tell the shader we bound the texture to texture unit 0
+  gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+
+
 
   {
     const offset = 0;
-
-    gl.drawArrays(gl.POINTS, offset, SIZE);
+    gl.drawArrays(gl.TRIANGLES, offset, SIZE * 6);
   }
 }
 
@@ -646,7 +788,7 @@ function initBuffers(gl) {
 
 
   gl.bufferData(gl.ARRAY_BUFFER,
-                new Float32Array(position),
+                new Float32Array(positionsData),
                 gl.STATIC_DRAW);
 
   //let colorBuffer = gl.createBuffer();
@@ -668,11 +810,16 @@ function initBuffers(gl) {
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
 
+  const textureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
 
 
   return {
     position: positionBuffer,
     color: colorBuffer,
+    textureCoord: textureCoordBuffer,
   };
 }
 
